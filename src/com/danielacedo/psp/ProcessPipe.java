dcb0be8695817313b75a051a75bf7f590f939713
Daniel Acedo Calderon <DanielExMachina0451@gmail.com>
2016-10-07 10:52:51 +0200

    Refactorized and commented the process

--- src/com/danielacedo/psp/ProcessPipe.java
+++ src/com/danielacedo/psp/ProcessPipe.java
@@ -12,46 +12,47 @@ public class ProcessPipe {
 
 	public static void main(String[] args) {
 		try {
-			Process ls = new ProcessBuilder("cmd","/c","dir").start();
-			Process grep = new ProcessBuilder("cmd", "/c", "find", "\"@\"").start();
+			Process ls = new ProcessBuilder("ls","-la").start();
+			Process grep = new ProcessBuilder("grep", "@").start();
 			
-			BufferedReader reader = new BufferedReader(new InputStreamReader(ls.getInputStream(), "UTF-8"));
+			//Connect ls output to a reader and wait for it to finish
+			BufferedReader readerA = new BufferedReader(new InputStreamReader(ls.getInputStream(), "UTF-8"));
+			//Connect grep input to writer
+			BufferedWriter writerB = new BufferedWriter(new OutputStreamWriter(grep.getOutputStream(), "UTF-8"));
+			
 			int lsCode = ls.waitFor();
 			
 			if(lsCode != 0){
-				System.out.println("El programa ls se ejecutó con error. Saliendo...");
+				System.out.println("El programa ls se ejecutÃ³ con error. Saliendo...");
 				System.exit(-1);
 			}
 			
-			String linea = null;
-			StringBuilder output = new StringBuilder();
+			//Start reading ls output and write it to grep as it is being read
+			String outputA = null;
 			
-			while ((linea = reader.readLine()) != null){
-				output.append(linea+"\n");
+			while ((outputA = readerA.readLine()) != null){
+				writerB.write(outputA+"\n");
 			}
 			
-			
-			reader.close();
-			
-			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(grep.getOutputStream(), "UTF-8"));
-			writer.write(output.toString());
-			writer.flush();
-			writer.close();
+			readerA.close();
+			writerB.flush();
+			writerB.close();
 			
 			int grepCode = grep.waitFor();
 			
 			if(grepCode != 0){
-				System.out.println("El programa grep se ejecutó con error. Saliendo...");
+				System.out.println("El programa grep se ejecutÃ³ con error. Saliendo...");
 				System.exit(-1);
 			}
 			
-			reader = new BufferedReader(new InputStreamReader(grep.getInputStream(), "UTF-8"));
+			//Read grep output to stdout
+			readerA = new BufferedReader(new InputStreamReader(grep.getInputStream(), "UTF-8"));
 			
-			while ((linea = reader.readLine())!=null){
-				System.out.println(linea);
+			while ((outputA = readerA.readLine())!=null){
+				System.out.println(outputA);
 			}
 			
-			reader.close();
+			readerA.close();
 			
 		} catch (IOException | InterruptedException e) {
 			System.err.println("Error");
